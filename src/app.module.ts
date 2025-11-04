1f67aa5f1a72757daa2273f1dff5b776a76ce268
islam said <islam.said.dev@gmail.com>
2025-11-04 19:15:57 +0200

    Create user module Create user schema Create user service Connect to mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Connection } from 'mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(process.env.MONGO_URI as string, {
+      onConnectionCreate: (connection: Connection) => {
+        connection.on('connected', () => console.log('connected'));
+        connection.on('open', () => console.log('open'));
+        connection.on('disconnected', () => console.log('disconnected'));
+        connection.on('reconnected', () => console.log('reconnected'));
+        connection.on('disconnecting', () => console.log('disconnecting'));
+
+        return connection;
+      },
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
