14c0bab9ad9489dc3bc59b53d2b26a7fd55b2b5b
islam said <islam.said.dev@gmail.com>
2025-11-04 20:28:56 +0200

    Handle user sign up Hash password Send jwt token

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,14 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Connection } from 'mongoose';
+import { JwtModule } from '@nestjs/jwt';
+
+const MONGO_URI =
+  'mongodb://admin:password@127.0.0.1:27017/mydb?authSource=admin';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(process.env.MONGO_URI as string, {
+    MongooseModule.forRoot((process.env.MONGO_URI as string) || MONGO_URI, {
       onConnectionCreate: (connection: Connection) => {
         connection.on('connected', () => console.log('connected'));
         connection.on('open', () => console.log('open'));
@@ -19,6 +23,13 @@ import { Connection } from 'mongoose';
         return connection;
       },
     }),
+    JwtModule.register({
+      // the only two modules needs it so registered as global
+      global: true,
+      secret: process.env.JWT_SECRET || 'dev-secret',
+      signOptions: { expiresIn: '14d' },
+    }),
+
     UserModule,
     AuthModule,
   ],
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,39 @@
 import { Injectable } from '@nestjs/common';
 import { SignupDto } from './dto/sign-up.dto';
 import { SigninDto } from './dto/sign-in.dto';
+import { UserService } from 'src/user/user.service';
+import bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  signup(signupDto: SignupDto) {
-    return signupDto;
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService,
+  ) {}
+
+  async signup(signupDto: SignupDto) {
+    // Hash password
+    const hashed_password = await bcrypt.hash(signupDto.password, 10);
+
+    // Save user data with hashed password
+    const user = await this.userService.create({
+      ...signupDto,
+      password: hashed_password,
+    });
+
+    // create jwt token
+    const jwtPayload = {
+      sub: user.id,
+      email: user.email,
+    };
+    const token = this.jwtService.sign(jwtPayload);
+
+    return {
+      access_token: token,
+      user: user,
+      message: 'User added successfully',
+    };
   }
 
   signin(signinDto: SigninDto) {
